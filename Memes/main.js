e1da1892c524910f87e55d61b137da3f9f7726b7
Justin Choo <kchoosun@gmail.com>
2020-06-29 23:10:00 -0700

    images working

--- Memes/main.js
+++ Memes/main.js
@@ -1,37 +1,34 @@
-document.addEventListener("DOMContentLoaded", () =>{
-    let memeObj;
-    let memeCount = 0;
-    fetch("https://api.imgflip.com/get_memes")
-    .then(res => res.json())
-    .then(myMemes => {
-        memeObj = myMemes;
-        let firstMemeURL = memeObj.data.memes[memeCount].url;
-        console.log(firstMemeURL);
-        let firstmeme = document.createElement("img");
-        firstmeme.src = `${firstMemeURL}`;
-        document.body.appendChild(firstmeme);
+const generateRandomNumber = () => {
+  return Math.floor(Math.random() * 100) + 1;
+};
+
+const memeCount = generateRandomNumber();
+let memeURL;
+fetch('https://api.imgflip.com/get_memes')
+  .then((res) => res.json())
+  .then((myMemes) => {
+    memeURL = myMemes.data.memes[memeCount].url;
+    const images = document.querySelectorAll('img');
+    images.forEach((el) => {
+      el.src = `${memeURL}`;
     });
+    //   const firstmeme = document.createElement('img');
+    //   firstmeme.src = `${firstMemeURL}`;
+    //   document.body.appendChild(firstmeme);
+  });
 
-    document.onkeydown = function (e) {
-        ++memeCount;
-        let currentMeme = memeObj.data.memes[memeCount].url;
-        document.body.innerHTML = `<img src = "${currentMeme}">`;
-    }
+// document.onkeydown = function (e) {
+//   ++memeCount;
+//   // const currentMeme = memeObj.data.memes[memeCount].url;
+//   const image = document.querySelector('img');
+//   image.src = `${firstMemeURL}`;
+// };
 
-
-    // const optionsObj = {
-    //     method: 'POST',
-    //     credentials: "same-origin",
-    //     headers: {
-    //         "Content-Type": "application/json; carset=utf-8"
-    //     },
-    //     body:
-    // }
-})
-
-
-
-
-
-
-
+// const optionsObj = {
+//     method: 'POST',
+//     credentials: "same-origin",
+//     headers: {
+//         "Content-Type": "application/json; carset=utf-8"
+//     },
+//     body:
+// }
--- WritingInPen/main.js
+++
@@ -1,8 +0,0 @@
-// let contents = document.getElementById("contents"); //How did I know to look for "contents"
-// contents.parentNode.removeChild(contents);
-
-document.onkeydown = function (e) {
-    if (e.keyCode === 8 || e.keyCode === 46) {
-        return false;
-    }
-}
