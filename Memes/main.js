b80e437dedfc74f0c8f30576e8f0fd5ff54bd9d4
Mercer Stronck <stronck@chapman.edu>
2020-06-29 23:12:30 -0700

    committing my WIP before Justin's changes

--- Memes/main.js
+++ Memes/main.js
@@ -1,32 +1,30 @@
 document.addEventListener("DOMContentLoaded", () =>{
     let memeObj;
     let memeCount = 0;
+    let firstmeme;
     fetch("https://api.imgflip.com/get_memes")
     .then(res => res.json())
     .then(myMemes => {
         memeObj = myMemes;
         let firstMemeURL = memeObj.data.memes[memeCount].url;
         console.log(firstMemeURL);
-        let firstmeme = document.createElement("img");
+        firstmeme = document.createElement("img");
         firstmeme.src = `${firstMemeURL}`;
         document.body.appendChild(firstmeme);
     });
 
     document.onkeydown = function (e) {
         ++memeCount;
+        let currentMemeURL = memeObj.data.memes[memeCount].url;
         let currentMeme = memeObj.data.memes[memeCount].url;
-        document.body.innerHTML = `<img src = "${currentMeme}">`;
+        document.body.innerHTML = `<img src = "${currentMemeURL}">`;
     }
 
-
-    // const optionsObj = {
-    //     method: 'POST',
-    //     credentials: "same-origin",
-    //     headers: {
-    //         "Content-Type": "application/json; carset=utf-8"
-    //     },
-    //     body:
-    // }
+    document.onkeydown = function (e) {
+        if(e.keyCode === 88){
+            console.log(currentMeme)
+        }
+    }
 })
 
 
